519882a3930fd1c1468ffc0689eee4d9b2efeaba
lucat <lucat@DESKTOP-S2EE6FE.homenet.telecomitalia.it>
2020-06-10 15:50:45 +0200

    Caricamento degli url corretti

--- PageLoader/src/main/java/page_loaderF/MainPageLoader.java
+++ PageLoader/src/main/java/page_loaderF/MainPageLoader.java
@@ -24,7 +24,7 @@ public class MainPageLoader {
 
 	public static void main(String[] args) {
 		ArrayList<URL> urls = new ArrayList<URL>();
-     
+
 		if(!startConnection()) {
 			System.out.println("Non e' stato possibile connettersi correttamente al server");
 			System.exit(0);
@@ -93,52 +93,55 @@ public class MainPageLoader {
 
 
 	private static void urlList(ArrayList<URL> urls) {
+		Scanner scan2 = new Scanner(System.in);
 		System.out.println("Digitare URL: \n");
 		String url;
-		do {
-			url =scan.nextLine();
+		while(true) {
+			url =scan2.nextLine();
+			if(url.equals("exit"))
+					break;
 			if( isValidURL(url)!=null ) {
 				try {
 					urls.add(new URL(url));
 					System.out.println("URL addato correttamente");
-					} catch (MalformedURLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				} else {
-					System.out.print(" \nInserire un URL valido: ");
-					url = scan.nextLine();
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
+
+			} else {
+				System.out.print(" \nInserire un URL valido: ");
+				url = scan.nextLine();
 			}
-		
-		while(!url.equals("exit"));
-}
-		
 
-	private static void closeProgram() {
-
+		}
 	}
-	
-	private static void createNewThread(ArrayList<URL> urlList) {
-		for(int index = 0; index < urlList.size(); index++) {
-			new PageLoader("F"+countThread,pageloader,urlList.get(index));
+
+
+		private static void closeProgram() {
+
 		}
 
-	}
-	
-	private static URL isValidURL(String urlStr) {
-		try {
-			if(urlStr.isEmpty()) {
+		private static void createNewThread(ArrayList<URL> urlList) {
+			for(int index = 0; index < urlList.size(); index++) {
+				new PageLoader("F"+countThread,pageloader,urlList.get(index));
+			}
+
+		}
+
+		private static URL isValidURL(String urlStr) {
+			try {
+				if(urlStr == null) {
+					return null;
+				}else {
+					URL url = new URL(urlStr);
+					return url;
+				}
+			}catch(MalformedURLException e ){
+				e.printStackTrace();
 				return null;
-			}else {
-				URL url = new URL(urlStr);
-				return url;
 			}
-		}catch(MalformedURLException e ){
-			return null;
 		}
 	}
-}
 
 
