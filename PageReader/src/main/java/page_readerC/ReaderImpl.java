1a946f397cd406efa10136238a96e8dddeaf33f4
lucat <lucat@DESKTOP-S2EE6FE.homenet.telecomitalia.it>
2020-06-10 16:12:34 +0200

    Commit pt.2

--- PageReader/src/main/java/page_readerC/ReaderImpl.java
+++ PageReader/src/main/java/page_readerC/ReaderImpl.java
@@ -17,17 +17,30 @@ public class ReaderImpl extends UnicastRemoteObject implements ReaderInterface,R
 	 */
 	private static final long serialVersionUID = 1L;
 	private ObservableInterface observable;
+	private int index = 0;
 
 	public ReaderImpl(ObservableInterface observable) throws RemoteException{
+		
 		this.observable = observable;
 
 	}
 
 	public void run() {
-
+		int random = (int) (Math.random()+10);
 		try {
 			System.out.println(" Il pageReader " +Thread.currentThread().getName() + " Si sta abbonando ");
-			observable.subscribe(this);				
+			observable.subscribe(this);	
+			while(random >=0 && random <6) {
+				observable.unsubscribe(this);
+				System.out.println("thread :"+Thread.currentThread().getName()+" si è arrestato");
+				random = (int) (Math.random()*10);
+			}
+			try {
+				Thread.currentThread().sleep(10000);		
+			}
+			catch(InterruptedException ie) {
+				ie.printStackTrace();
+			}
 			System.out.println(" Il pageReader " +Thread.currentThread().getName() + " Si e' abbonato");
 
 		} catch (RemoteException e) {
@@ -43,25 +56,12 @@ public class ReaderImpl extends UnicastRemoteObject implements ReaderInterface,R
 	 */
 	@Override
 	public void update(Page page, URL url) throws RemoteException {
-		if(page == null)
-			System.out.println("pagina inesistente");
-		int random = (int) (Math.random()+10);
-		int index = 0;
+		
 		System.out.println(Thread.currentThread().getName()+" sta per leggere dall'url :"+url.toString());
-		while(random >=0 && random <6) {
-			System.out.println(page.getInfo(index));
-			index++;
-			try {
-				Thread.currentThread().sleep(10000);		//il try catch l'ho messo alla fine così in caso che faccia l'unsuscribe viene catturata la flag e arrestato
-			}
-			catch(InterruptedException ie) {
-				ie.printStackTrace();
-			}
-			random = (int) (Math.random()+10);
-		}
-
-		observable.unsubscribe(this);
-		System.out.println("thread :"+Thread.currentThread().getName()+" si è arrestato");
+		if(page == null)
+			System.out.println("pagina nulla");
+		System.out.println(page.getInfo(index));
+		index++;
 
 	}
 
